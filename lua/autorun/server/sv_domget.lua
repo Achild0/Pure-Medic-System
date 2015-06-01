9a8d73db0ef67bada5b1d7019e94a5a9b5493964
Achild0 <lesbatsvirgile@hotmail.com>
2015-06-02 01:09:56 +0200

    Changement du client vers serveur

--- lua/autorun/server/sv_domget.lua
+++ lua/autorun/server/sv_domget.lua
@@ -1,9 +1,29 @@
-util.AddNetworkString( "wdtaken" )
+AddCSLuaFile()
+include("autorun/pure_med_config.lua")
 hook.Add("PlayerHurt","Ply-H",function( vict, attckr, hrem, dmgt )
   if vict:IsPlayer() then
-    print(vict:LastHitGroup())
-    net.Start("wdtaken")
-      net.WriteInt(vict:LastHitGroup(),4)
-    net.Send(vict)
+      hitgrp = vict:LastHitGroup()
+      print(hitgrp)
+      if hitgrp == 1 then
+        print("La tête est touchée")
+      elseif hitgrp == 4 then
+        print("Bras Gauche touché")
+      elseif hitgrp == 5 then
+        print("Bras Droit touché")
+      elseif hitgrp == 7 then
+        print("Jambe Droite touchée")
+        vict:SetRunSpeed(PMED.hitrnspeed)
+        vict:SetWalkSpeed(PMED.hitwkspeed)
+      elseif hitgrp == 6 then
+        print("Jambe Gauche touchée")
+        vict:SetRunSpeed(PMED.hitrnspeed)
+        vict:SetWalkSpeed(PMED.hitwkspeed)
+      elseif hitgrp == 2 then
+        print("Torse touché")
+      elseif hitgrp == 3 then
+        print("Estomac touché")
+      else
+        print("Achild tu t'es chié du con")
+      end
   end
 end)
