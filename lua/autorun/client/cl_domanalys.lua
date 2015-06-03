7b65ed012bccfc746c3a8eeb355ab5c54cb4155d
Achild0 <lesbatsvirgile@hotmail.com>
2015-06-03 02:10:02 +0200

    ajout des localisation blessure + effets

--- lua/autorun/client/cl_domanalys.lua
+++ lua/autorun/client/cl_domanalys.lua
@@ -1,4 +1,29 @@
 AddCSLuaFile()
 include("autorun/pure_med_config.lua")
 local btm = 70
-ply = LocalPlayer()
+local ply = LocalPlayer()
+local hhit = false
+local tab = {
+	["$pp_colour_addr"] = 0,
+	["$pp_colour_addg"] = 0,
+	["$pp_colour_addb"] = 0,
+	["$pp_colour_brightness"] = 0,
+	["$pp_colour_contrast"] = 0,
+	["$pp_colour_colour"] = 0,
+	["$pp_colour_mulr"] = 0,
+	["$pp_colour_mulg"] = 0,
+	["$pp_colour_mulb"] = 0
+}
+net.Receive("screench", function(len)
+	if net.ReadBool() == true then
+		hhit = true
+	end
+	return hhit
+end)
+
+hook.Add("RenderScreenspaceEffects","h_hit",function()
+	if hhit == true then
+		DrawMotionBlur( 0.3, 0.9, 0.02 )
+		DrawBloom( 0.4, 2, 9, 9, 1, 1, 1, 1, 1 )
+	end
+end)
--- lua/autorun/server/sv_domget.lua
+++ lua/autorun/server/sv_domget.lua
@@ -1,27 +1,58 @@
 AddCSLuaFile()
 include("autorun/pure_med_config.lua")
-hook.Add("PlayerHurt","Ply-H",function( vict, attckr, hrem, dmgt )
+
+util.AddNetworkString( "screench" )
+
+hhits = {}
+bghits = {}
+bdhits = {}
+jdhits = {}
+jghits = {}
+thits = {}
+ehits = {}
+
+hook.Add("PlayerHurt","Ply_H",function( vict, attckr, hrem, dmgt )
   if vict:IsPlayer() then
       hitgrp = vict:LastHitGroup()
       print(hitgrp)
       if hitgrp == 1 then
         print("La tête est touchée")
+        util.ScreenShake( vict:GetPos(), 3, 5, 50, 1 )
+        net.Start("screench")
+          net.WriteBool(true)
+        net.Send(vict)
+        table.insert(hhits, "Blessure par balle")
+        print(hhits)
       elseif hitgrp == 4 then
         print("Bras Gauche touché")
+        util.ScreenShake( vict:GetPos(), 5, 2, 30, 1 )
+        table.insert(bghits, "Blessure par balle")
+        print(bghits)
       elseif hitgrp == 5 then
         print("Bras Droit touché")
+        util.ScreenShake( vict:GetPos(), 5, 2, 30, 1 )
+        table.insert(bdhits, "Blessure par balle")
+        print(bdhits)
       elseif hitgrp == 7 then
         print("Jambe Droite touchée")
         vict:SetRunSpeed(PMED.hitrnspeed)
         vict:SetWalkSpeed(PMED.hitwkspeed)
+        table.insert(jdhits, "Blessure par balle")
+        print(jdhits)
       elseif hitgrp == 6 then
         print("Jambe Gauche touchée")
         vict:SetRunSpeed(PMED.hitrnspeed)
         vict:SetWalkSpeed(PMED.hitwkspeed)
+        table.insert(jghits, "Blessure par balle")
+        print(jghits)
       elseif hitgrp == 2 then
         print("Torse touché")
+        table.insert(thits, "Blessure par balle")
+        print(thits)
       elseif hitgrp == 3 then
         print("Estomac touché")
+        table.insert(ehits, "Blessure par balle")
+        print(ehits)
       else
         print("Achild tu t'es chié du con")
       end
